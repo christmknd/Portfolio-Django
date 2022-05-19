ca26909dc1bb70dd43e94384619159b38f68733c
christmknd <christmokonda@gmail.com>
2022-05-19 17:00:29 +0200

    ADD success page after contact message

--- app/contact/urls.py
+++ app/contact/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from contact import views
 
 urlpatterns = [
-    path('',views.contact, name='contact')
+    path('',views.contact, name='contact'),
+    path('sucess', views.sucess, name='sucess')
 ]
--- app/contact/views.py
+++ app/contact/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
-
-# Create your views here.
+from . models import Contact
+from . forms import ContactForm
+from django.http import HttpResponseRedirect , HttpResponse
 
 def contact(request):
-    return render(request,'contact.html',{})
+    form_class = ContactForm
+    form = form_class
+
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            name = request.POST.get('name')
+            email = request.POST.get('email')
+            subject = request.POST.get('subject')
+
+            #envoie des données dans le base de donées admin 
+            contact = Contact
+            contact.name = name
+            contact.email= email
+            contact.subject = subject
+            contact.save
+            return HttpResponse('<p>sucess</p>')
+    return render(request,'contact.html', {'form' : form})
+
+def sucess(request): 
+    return render(request,'sucess.html')
